7912bfaaf2a57e03347113133b7757dead231bb8
daniyal ali <daniyalali12568@gmail.com>
2024-02-02 02:22:33 +0500

    update-functionality

--- src/pages/DashboardContact/DashboardContact.jsx
+++ src/pages/DashboardContact/DashboardContact.jsx
@@ -43,6 +43,13 @@ const DashboardContact = () => {
   let buttonText = {
     createcontact: "Create contact",
   };
+  // ALl POST STATE
+  const [allData,setAllData] = useState([])
+  console.log(allData);
+  //DELID
+  const [delId,setDelId] = useState(null)
+  console.log(delId);
+  //DELLOADING
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -50,11 +57,14 @@ const DashboardContact = () => {
   const [imageFile, setImageFile] = useState(null);
   const [loading , setLoading]  = useState(false)
 
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(!open);
 
-  const [del, setDel] = React.useState(false);
-  const handleDel = () => setDel(!del);
+  const [del, setDel] = useState(false);
+  const handleDel = (id) => {
+ setDelId(id);  
+ setDel(!del);
+  }
 
   const [update, setUpdate] = useState(false);
   const handleupdate = () => setUpdate(!update);
@@ -87,9 +97,14 @@ const DashboardContact = () => {
 
   useEffect(() => {
     dashboardValid();
+    allPostHandler()
   }, []);
 
-  //post submit handler
+  useEffect(() => {
+    allPostHandler()
+  }, []);
+
+  //POSTSUBMIT
   const postSubmitHandler = async (e) => {
     e.preventDefault();
     
@@ -115,9 +130,57 @@ const DashboardContact = () => {
       setLoading(false)
     } catch (error) {
       console.error('error', error);
+      setLoading(false)
     }
   };
-
+  // ALLPOST
+  const allPostHandler = async () => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      console.error("Token not found in localStorage");
+      return;
+    }
+    try {
+      const res = await axios.get(
+        "http://localhost:8000/api/allpost",
+        {
+          headers: {
+            authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      setAllData(res.data.Data);
+    } catch (error) {
+      console.error("error",error);
+    }
+  };
+  
+  
+ // DELPOST
+ const delPostHandler = async(id)=>{
+  const token = localStorage.getItem("token")
+  if(!token){
+    console.log("Token not found in local storage");
+    console.log(id);
+  }
+  try{
+   
+    const res = await axios.delete(
+      `http://localhost:8000/api/delpost/${id}`,
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    console.log(res.data);
+    allPostHandler()
+  }catch(err){
+    console.log(err);
+  }
+ }
   return (
     <>
       <NavbarSimple />
@@ -206,7 +269,6 @@ const DashboardContact = () => {
                 Create List
               </Button>)
               }
-             
             </DialogFooter>
             </form>
           </Dialog>
@@ -250,17 +312,18 @@ const DashboardContact = () => {
                 </tr>
               </thead>
               <tbody>
-                {TABLE_ROWS.map(({ name, job, date }, index) => {
+                
+                {allData.map(({ name, email, contact ,times ,imageUrl, _id }, index) => {
                   const isLast = index === TABLE_ROWS.length - 1;
                   const classes = isLast
                     ? "p-4"
                     : "p-4 border-b border-blue-gray-50";
 
                   return (
-                    <tr key={name}>
+                    <tr key={index}>
                       <td className={classes}>
                         <Avatar
-                          src="https://docs.material-tailwind.com/img/face-2.jpg"
+                          src={imageUrl}
                           alt="avatar"
                           color="purple"
                           withBorder={true}
@@ -284,7 +347,7 @@ const DashboardContact = () => {
                           color="blue-gray"
                           className="font-normal"
                         >
-                          {job}
+                          {email}
                         </Typography>
                       </td>
 
@@ -294,7 +357,7 @@ const DashboardContact = () => {
                           color="blue-gray"
                           className="font-normal"
                         >
-                          {job}
+                          {contact}
                         </Typography>
                       </td>
 
@@ -304,7 +367,7 @@ const DashboardContact = () => {
                           color="blue-gray"
                           className="font-normal"
                         >
-                          {date}
+                          {times}
                         </Typography>
                       </td>
                       <td className={classes}>
@@ -327,7 +390,7 @@ const DashboardContact = () => {
                           variant="small"
                           color="red"
                           className="font-medium"
-                          onClick={() => handleDel()}
+                          onClick={() => handleDel(_id)}
                         >
                           Delete
                         </Typography>
@@ -335,10 +398,9 @@ const DashboardContact = () => {
                     </tr>
                   );
                 })}
-              </tbody>
-            </table>
-          </Card>
-          <Dialog open={update} size="xs" handler={handleupdate}>
+
+                {/* EDIT pop */}
+            <Dialog open={update} size="xs" handler={handleupdate}>
             <div className="flex items-center justify-between">
               <DialogHeader className="flex flex-col items-start">
                 <Typography className=" text-2xl font-[10px]" variant="h2">
@@ -392,42 +454,46 @@ const DashboardContact = () => {
             </DialogFooter>
           </Dialog>
 
-          {/* Delete pop */}
-          <Dialog size={"xs"} open={del} handler={handleDel}>
-            <div className="w-14 text-center my-2 mx-auto">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="2"
-                stroke="currentColor"
-                className="w-14 h-14"
-                color="#FF2E2E"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                />
-              </svg>
-            </div>
-            <DialogBody>Are you sure you want to delete this item</DialogBody>
-            <div className="flex justify-center">
-              <DialogFooter>
-                <Button
-                  variant="text"
-                  color="purple"
-                  onClick={handleDel}
-                  className="mr-1"
-                >
-                  <span>Cancel</span>
-                </Button>
-                <Button variant="gradient" color="red" onClick={handleDel}>
-                  Delete
-                </Button>
-              </DialogFooter>
-            </div>
-          </Dialog>
+          {/* DELETE pop */}
+
+{
+  delId &&  <Dialog size={"xs"} open={del} handler={handleDel}>
+  <div className="w-14 text-center my-2 mx-auto">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke="currentColor"
+      className="w-14 h-14"
+      color="#FF2E2E"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+      />
+    </svg>
+  </div>
+  <DialogBody>Are you sure you want to delete this item</DialogBody>
+  <div className="flex justify-center">
+    <DialogFooter>
+      <Button variant="text" color="purple" onClick={handleDel} className="mr-1" >
+        <span>Cancel</span>
+      </Button>
+      <Button variant="gradient" color="red"  onClick={() => { delPostHandler(delId); handleDel(); }} >
+        Delete
+      </Button>
+    </DialogFooter>
+  </div>
+</Dialog>
+}
+
+          
+              </tbody>
+            </table>
+          </Card>
+         
         </div>
       </div>
       <div></div>
