afc7366b0947420bc5740066e598dbfedd21fcaf
daniyal ali <daniyalali12568@gmail.com>
2024-02-06 20:25:44 +0500

    complet

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Register from './pages/Register/Register.jsx'
 import Login from './pages/Login/Login.jsx'
 import Home from "./pages/Home/Home.jsx"
 import PageNotfound from './pages/Notfound/PageNotfound.jsx'
-import Protectedroutes from '../routes/protectedroutes.jsx'
+import Protectedroutes from '../routes/Protectedroutes.jsx'
 import AuthenticateRoute from '../routes/AuthenticateRoute.jsx'
 
 function App() {
