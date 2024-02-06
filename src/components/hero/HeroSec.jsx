7fea45dba6208d70ef6af34f59dcc8886f6958e1
daniyal ali <daniyalali12568@gmail.com>
2024-02-07 00:37:58 +0500

    complet

--- src/components/hero/HeroSec.jsx
+++ src/components/hero/HeroSec.jsx
@@ -1,37 +1,69 @@
 import React from "react";
 import Buttonseemore from "../button/Buttonseemore";
+import { Link } from "react-router-dom";
 
 const HeroSec = () => {
-  let buttonText={
-    createcontact:"See More"
-}
+ 
   return (
-    <div className="w-[95%] flex justify-center items-center  mx-auto sm:h-40 lg:h-[27rem]">
-      <div className="sm:w-[80%] md:w-32 lg:w-[60%] h-auto  mx-auto whitespace-normal">
-        <div className=" text-center">
-          <h1
-            className="lg:text-5xl  sm:p-2  tracking-tight font-bold  indent-1.5 text-center sm:leading-4 md:leading-10 lg:leading-12"
-            style={{ lineHeight: "1.12em" }}
-          >
-            Manage Your Contacts With
-            <span className="text-purple-600"> Contact Book</span>
-          </h1>
-        </div>
+   
 
-        {/* Add a paragraph with increased line height */}
-        <div>
-          <p className="mx-auto text-center max-w-[95%] sm:max-w-[90%] lg:max-w-[80%] xl:max-w-[75%] 2xl:max-w-[70%] align-center indent-4 leading-4 sm:leading-5 md:leading-5 lg:leading-6 text-sm sm:text-base lg:base xl:text-base 2xl:text-base ">
-            Unleash the power of simplicity with our Contact Book app where
-            organization meets effortless connection. Your contacts, your way,
-            always at your fingertips
-          </p>
-        </div>
-        <div className="text-center mt-3">
-          <Buttonseemore buttonText={buttonText} />
-        </div>
-      </div>
+<section className="bg-white dark:bg-gray-900">
+  <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+    <a
+      href="#"
+      className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-800 bg-gray-100 rounded-full dark:bg-dark-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700 "
+      
+    >
+      <span className="text-xs bg-purple-400 rounded-full text-white px-4 py-1.5 mr-3">
+        New
+      </span>
+      <span className="sm:text-[0.4rem] md:text-[0.8rem] lg:text-[0.8rem] xs:text-[0.6rem]">
+        Flexible and easy to maintain check it now
+      </span>
+      <svg
+        className="ml-2 w-5 h-5"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fillRule="evenodd"
+          d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+          clipRule="evenodd"
+        />
+      </svg>
+    </a>
+    <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+    Manage Your Contacts With  <span className="text-purple-600"> Contact Book</span>
+    </h1>
+    <p className="mb-8 text-lg font-normal text-gray-500 lg:text-[1rem] sm:px-12 xl:px-48 dark:text-gray-500 indent-4">
+    Unleash the power of simplicity with our Contact Book app where organization meets effortless connection. Your contacts, your way, always at your fingertips
+    </p>
+    <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+
+      {
+        localStorage.getItem("token") ? (  <Link
+          to="/dashboard"
+          className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center  rounded-lg border border-purple-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 text-purple-600 dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+        >
+          see More
+        </Link>) : (  <Link
+        to="/login"
+        className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center  rounded-lg border border-purple-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 text-purple-600 dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+      >
+        see More
+      </Link>)
+      }
+   
+    
     </div>
-  );
+    
+  </div>
+</section>
+
+
+
+  )
 };
 
 export default HeroSec;
